10c862a5d26a8ab09a0dc50e2a665f4df27ad416
Koray İNÇKİ <koray.incki@ozu.edu.tr>
2020-12-03 15:40:25 +0300

    somechange

---
+++ retet/src/tdd/CalcParameterResolver.java
@@ -0,0 +1,27 @@
+package tdd;
+/**
+ * 
+ */
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+/**
+ * @author korayincki
+ *
+ */
+public class CalcParameterResolver implements ParameterResolver{
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, 
+			ExtensionContext extensionContext) throws ParameterResolutionException {
+				return parameterContext.getParameter().getType() == Calculator.class;
+		
+	}
+
+	@Override
+	public Object resolveParameter(ParameterContext arg0, ExtensionContext arg1) throws ParameterResolutionException {
+		return new Calculator();
+	}
+}
---
+++ retet/src/tdd/Calculator.java
@@ -0,0 +1,15 @@
+package tdd;
+
+public class Calculator {
+	
+	public Long add(long...operands) {
+		long result=0;
+		
+		for (long number:operands) {
+			result += number;
+		}
+		
+		return result;
+	}
+
+}
